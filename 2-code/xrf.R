8c620f416a93f6e7478bb790fddbf9761262a0d4
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2026-03-20 12:07:12 -0700

    update XRF

--- 2-code/xrf.R
+++ 2-code/xrf.R
@@ -11,6 +11,7 @@ xrf_long =
   xrf_data %>% 
   mutate_all(as.character) %>% 
   pivot_longer(cols = -c(CRESS_ID), names_to = "analyte", values_to = "XRF_ugg") %>% 
+  filter(!analyte %in% c("U", "Th")) %>% 
   mutate(XRF_ugg = as.numeric(XRF_ugg),
          XRF_ugg = replace_na(XRF_ugg, 0))
 
